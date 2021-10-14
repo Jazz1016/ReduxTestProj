8fae13377b97a332631e27b5f8112572cb903406
James L <59751464+Jazz1016@users.noreply.github.com>
2021-10-14 15:37:16 -0600

    implemented useReducer

--- redux-test/src/App.js
+++ redux-test/src/App.js
@@ -1,25 +1,36 @@
 import React, { useState, useReducer } from "react";
 
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
+
 function reducer(state, action) {
-  return { count: state + 1 };
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 };
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 }
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, 0);
-  const [count, setCount] = useState(0);
 
   function increment() {
-    setCount((prevCount) => prevCount + 1);
+    dispatch({ type: ACTIONS.INCREMENT });
   }
 
   function decrement() {
-    setCount((prevCount) => prevCount - 1);
+    dispatch({ type: ACTIONS.DECREMENT });
   }
 
   return (
     <>
       <button onClick={decrement}>-</button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}>+</button>
     </>
   );
